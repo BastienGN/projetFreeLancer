32bc3a0b4778dbdc7d704dea96f7c6761244371f
root <root@localhost.localdomain>
2022-01-18 10:56:18 -0500

    la base : dépendances

---
+++ src/main/java/com/inti/FreeLancerApplication.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeLancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeLancerApplication.class, args);
+	}
+
+}
