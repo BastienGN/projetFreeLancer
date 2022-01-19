622e30ee2dc6c4312597f4fa1aa663eb1cc8a061
root <root@localhost.localdomain>
2022-01-19 10:00:57 +0100

    Class Utilisateur + Role

---
+++ src/main/java/com/inti/controllers/RoleController.java
@@ -0,0 +1,55 @@
+package com.inti.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.inti.entities.Role;
+import com.inti.services.interfaces.IRoleService;
+
+@RestController
+@CrossOrigin
+public class RoleController {
+
+	@Autowired
+	IRoleService roleService;
+
+	@RequestMapping(value = "roles", method = RequestMethod.GET)
+	public List<Role> findAll() {
+		return roleService.findAll();
+	}
+
+	@RequestMapping(value = "roles/{idR}", method = RequestMethod.GET)
+	public Role findOne(@PathVariable("idR") Long idRole) {
+		return roleService.findOne(idRole);
+	}
+
+	@RequestMapping(value = "roles/{idR}/{libelle}", method = RequestMethod.GET)
+	public Role findByIdRoleAndLibelle(@PathVariable("idR") Long idRole, @PathVariable("libelle") String libelle) {
+		return roleService.findByIdRoleAndLibelle(idRole, libelle);
+	}
+
+	@RequestMapping(value = "roles", method = RequestMethod.POST)
+	public Role saveRole(@RequestBody Role role) {
+		return roleService.save(role);
+	}
+
+	@RequestMapping(value = "roles/{idR}", method = RequestMethod.PUT)
+	public Role updateRole(@PathVariable(value = "idR") Long idRole, @RequestBody Role role) {
+		Role currentRole = roleService.findOne(idRole);
+		currentRole.setLibelle(role.getLibelle());
+		return roleService.save(currentRole);
+	}
+
+	@RequestMapping(value = "roles/{idR}", method = RequestMethod.DELETE)
+	public void deleteRole(@PathVariable(value = "idR") Long idRole) {
+		roleService.delete(idRole);
+	}
+
+}
--- src/main/java/com/inti/entities/Utilisateur.java
+++ src/main/java/com/inti/entities/Utilisateur.java
@@ -1,5 +1,144 @@
 package com.inti.entities;
 
-public class Utilisateur {
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Utilisateur implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idUtilisateur;
+	private String nomUtilisateur;
+	private String prenomUtilisateur;
+	@Column(unique = true)
+	private String username;
+	private String password;
+	private Adresse adresse;
+	private Date dateNaissance;
+	private String adresseMail;
+	private Long telephone;
+	private boolean enabled = true;
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "Profil", joinColumns = @JoinColumn(name = "id_utilisateur", referencedColumnName = "idUtilisateur"), inverseJoinColumns = @JoinColumn(name = "id_role", referencedColumnName = "idRole"))
+	private Set<Role> roles = new HashSet<>();
+
+	public Utilisateur() {
+	}
+
+	public Utilisateur(String nomUtilisateur, String prenomUtilisateur, String username, String password) {
+		this.nomUtilisateur = nomUtilisateur;
+		this.prenomUtilisateur = prenomUtilisateur;
+		this.username = username;
+		this.password = password;
+	}
+
+	public Long getIdUtilisateur() {
+		return idUtilisateur;
+	}
+
+	public void setIdUtilisateur(Long idUtilisateur) {
+		this.idUtilisateur = idUtilisateur;
+	}
+
+	public String getNomUtilisateur() {
+		return nomUtilisateur;
+	}
+
+	public void setNomUtilisateur(String nomUtilisateur) {
+		this.nomUtilisateur = nomUtilisateur;
+	}
+
+	public String getPrenomUtilisateur() {
+		return prenomUtilisateur;
+	}
+
+	public void setPrenomUtilisateur(String prenomUtilisateur) {
+		this.prenomUtilisateur = prenomUtilisateur;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Adresse getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(Adresse adresse) {
+		this.adresse = adresse;
+	}
+
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+
+	public String getAdresseMail() {
+		return adresseMail;
+	}
+
+	public void setAdresseMail(String adresseMail) {
+		this.adresseMail = adresseMail;
+	}
+
+	public Long getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(Long telephone) {
+		this.telephone = telephone;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+	@Override
+	public String toString() {
+		return "Utilisateur [idUtilisateur=" + idUtilisateur + ", nomUtilisateur=" + nomUtilisateur
+				+ ", prenomUtilisateur=" + prenomUtilisateur + ", username=" + username + ", password=" + password
+				+ ", dateNaissance=" + dateNaissance + ", adresseMail=" + adresseMail + ", telephone=" + telephone
+				+ ", enabled=" + enabled + ", roles=" + roles + "]";
+	}
 
 }
