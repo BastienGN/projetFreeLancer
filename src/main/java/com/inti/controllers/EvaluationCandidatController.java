01a76c24885a65d9f173d777108dc77fc3f34513
marieberger38 <marieberger139@gmail.com>
2022-01-21 14:36:52 +0100

    21/01 14:36

--- src/main/java/com/inti/controllers/EvaluationCandidatController.java
+++ src/main/java/com/inti/controllers/EvaluationCandidatController.java
@@ -3,14 +3,18 @@ package com.inti.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.inti.entities.EvaluationCandidat;
 import com.inti.services.interfaces.IEvaluationCandidatService;
 
+@RestController
+@CrossOrigin
 public class EvaluationCandidatController {
 	@Autowired
 	IEvaluationCandidatService evaluationCandidatService;
